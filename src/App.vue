001d585b5591d3bfdcf5bc7e44ea312a5018e18a
13886711507sjk <www.2645968117@qq.com>
2020-09-28 09:07:13 +0800

    上传试图

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
   <div id="app">
-    
+    <div class="container">
+      <div>
+        <div class="col-2">
+          <index />
+        </div>
+        <div class="col-10">
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
 
+<script>
+import index from "./views/index.vue";
+
+export default {
+  name: "App",
+  components: {
+    index,
+  },
+};
+</script>
+
+<style>
 </style>
